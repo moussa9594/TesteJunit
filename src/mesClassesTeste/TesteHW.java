f3954697c5ad2bbc047f5160b34b650aa31ad7a5
moussa <moussa9594@gmail.com>
2019-11-14 10:07:13 +0000

    modification du fichier TesteHW

--- src/mesClassesTeste/TesteHW.java
+++ src/mesClassesTeste/TesteHW.java
@@ -1,24 +1,21 @@
-package mesClassesTeste;
-
-import static org.junit.Assert.assertNull;
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
-import mesClasses.HelloWord;
-
-class TesteHW {
-
-	@Test
-	void testGetMessage() {
-		assertNotNull(HelloWord.getMessage("moussa"));
-	}
-
-	@Test
-	void testSum() {
-		assertNotNull(HelloWord.sum(2, 3));
-	}
-
-	
-
-}
+/*
+ * package mesClassesTeste;
+ * 
+ * import static org.junit.Assert.assertNull; import static
+ * org.junit.jupiter.api.Assertions.*;
+ * 
+ * import org.junit.jupiter.api.Test;
+ * 
+ * import mesClasses.HelloWord;
+ * 
+ * class TesteHW {
+ * 
+ * @Test void testGetMessage() { assertNotNull(HelloWord.getMessage("moussa"));
+ * }
+ * 
+ * @Test void testSum() { assertNotNull(HelloWord.sum(2, 3)); }
+ * 
+ * 
+ * 
+ * }
+ */
